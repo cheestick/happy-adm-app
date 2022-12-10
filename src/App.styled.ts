0a10e1a52f382800fb4e8161a104cdab056dfdd2
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-12-10 21:23:52 +0200

    init

---
+++ src/App.styled.ts
@@ -0,0 +1,42 @@
+import tw from "tailwind-styled-components"
+
+export const Hero = tw.section`
+  flex
+  place-content-center
+  min-w-full
+  min-h-[30vh]
+  bg-lime-50
+`
+
+interface WrapperProps {
+  $center?: boolean
+}
+
+export const Wrapper = tw.div<WrapperProps>`
+  ${ ({ $center }) => $center
+    ? 'flex place-content-center flex-col'
+    : 'block' 
+  }
+  min-w-[calc(100vw-2rem)]
+  mx-auto
+  min-h-full
+`
+
+export const MainHeading = tw.h1`
+  text-8xl
+  font-bold
+  underline
+  
+  text-center
+  align-middle
+
+  last:-mt-[5.8rem]
+  last:-ml-3
+  last:text-indigo-300
+  hover:last:text-indigo-400
+
+  text-indigo-200
+  hover:text-indigo-300
+
+  select-none
+`
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {Hero, Wrapper, MainHeading} from './App.styled'
+
+function App() {
+  return (
+    <Hero>
+      <Wrapper >
+        <MainHeading>Vite React TS</MainHeading>
+        <MainHeading>Vite React TS</MainHeading>
+      </Wrapper>
+    </Hero>
+  )
+}
+
+export default App
