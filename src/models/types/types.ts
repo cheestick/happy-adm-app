b52224e8f1090c4a4a680172a5b75065c7edafe5
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-12-19 11:46:53 +0200

    models types WIP continued

--- src/models/types/types.ts
+++ src/models/types/types.ts
@@ -10,22 +10,43 @@ export interface ICableLine {
   type: TCableType
 }
 
-interface ISet {
+export interface ISet {
   id: TDatabaseID
   status: 'busy' | 'idle' | 'prophylaxy'
   duration: number // minutes
   startTime: Date // hh:mm
   date: Date // yyyy:mm
+  trainer: IUser
+  riders: IUser[] // 1 or more riders/contacts
 }
 
 type TUserType = 'super' | 'admin' | 'trainer' | 'rider'
-
-interface IUser{
-  id: TDatabaseID
-  type: TUserType
+type TUserName = string
+type TNickName = string
+type TUserContacts = {
+  phone: string
+  email: string
+  telegram: string
 }
 
-interface IOrder {
+export interface IUser{
+  id: TDatabaseID
+  type: TUserType
+  username: TUserName
+  nickname: TNickName
+  contacts: TUserContacts
+}
+
+export interface IOrder {
   id: TDatabaseID
   set: ISet[]
 }
+
+export type TActivityType =
+  | 'wake'
+  | 'trip'
+  | 'snowboarding'
+  | 'skateboarding'
+  | 'trampoline'
+  | 'contest'
+  | 'yoga'
