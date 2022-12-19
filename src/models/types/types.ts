6ead5edfc0e8ebcd388c10a927aa9c784d12533c
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-12-19 11:30:59 +0200

    models types WIP

--- src/models/types/types.ts
+++ src/models/types/types.ts
@@ -1,3 +1,31 @@
-export interface ILine {
-  
+type TCableType = Uppercase<'obstacles' | 'flat'>
+type TDatabaseID = string
+type TCableTitle = string
+type TSignboard = string
+
+export interface ICableLine {
+  id: TDatabaseID
+  spotSignboard: TSignboard
+  title: TCableTitle
+  type: TCableType
+}
+
+interface ISet {
+  id: TDatabaseID
+  status: 'busy' | 'idle' | 'prophylaxy'
+  duration: number // minutes
+  startTime: Date // hh:mm
+  date: Date // yyyy:mm
+}
+
+type TUserType = 'super' | 'admin' | 'trainer' | 'rider'
+
+interface IUser{
+  id: TDatabaseID
+  type: TUserType
+}
+
+interface IOrder {
+  id: TDatabaseID
+  set: ISet[]
 }
