57948c1eb6ebec438058992bda491bb17ec2e21f
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-12-20 17:33:57 +0200

    types for work day entity was added. Lets think about it

--- src/models/types/types.ts
+++ src/models/types/types.ts
@@ -50,3 +50,44 @@ export type TActivityType =
   | 'trampoline'
   | 'contest'
   | 'yoga'
+
+//consider this
+
+type TStringOrNumber = string | number
+
+type TWorkDate = {
+    year: TStringOrNumber
+    month: TStringOrNumber
+    day: TStringOrNumber
+}
+
+type TTime = {
+  hours: TStringOrNumber // 5...22 for example
+  minutes: TStringOrNumber // 0 | 30 
+}
+
+// considered implementing this entity to operate actual Set slot range
+// needs to be displayed
+export interface IWorkDay {
+  id: TDatabaseID
+  createdAt: Date
+  updatedAt: Date
+  workDate: TWorkDate
+  from: TTime  // from 5.00 
+  till: TTime  // till 21.00
+  occupiedSlots: ISet[]  // list of full sets / set ids
+  vacantSlots: ISet[]  // list of empty sets / set ids
+}
+
+/** timetable format ??
+ *{ [startTime]: setID }
+ * 
+ * { 
+ *  "09:00": "111-bbb-ccc-ddd-eee"
+ *  "09:30": "222-bbb-ccc-ddd-eee"
+ *  "10:30": "333-bbb-ccc-ddd-eee"
+ *  "11:30": "444-bbb-ccc-ddd-eee"
+ * }
+ * */
+
+
